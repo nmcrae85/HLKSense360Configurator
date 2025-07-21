edef0bfa79527b9b2e6e63753afaea496d48fe35
nmcrae85 <32064932+nmcrae85@users.noreply.github.com>
2025-07-21 23:04:57 +0100

    Add files via upload

--- hlk2450-configurator/client/src/App.tsx
+++ hlk2450-configurator/client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,12 +6,28 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home.tsx";
 import NotFound from "@/pages/not-found";
 
+// Detect Home Assistant ingress base path
+function getBasePath() {
+  const pathname = window.location.pathname;
+  // Check if we're running under Home Assistant ingress
+  if (pathname.includes('/hassio/ingress/')) {
+    // Extract the ingress path up to and including the addon slug
+    const match = pathname.match(/^(\/hassio\/ingress\/[^\/]+)/);
+    return match ? match[1] : '';
+  }
+  return '';
+}
+
 function Router() {
+  const basePath = getBasePath();
+  
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base={basePath}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
